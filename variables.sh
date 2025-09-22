db80016c21c66606ef07edfe179c45d6e2dd77da
satish <vallem.pspk@gmail.com>
2025-09-22 21:40:37 +0530

    added something new

--- variables.sh
+++ variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PET1=dog
 PET2=cat
-echo "hello cat how are you"
-echo "hi dog iam great"
-echo "let play in the grounnd dog"
-echo "see mister cat I cannot play with you"
+echo "hello $PET2 how are you"
+echo "hi $PET1 iam great"
+echo "lets play in the ground $PET1"
+echo "see mister $PET2 I cannot play with you"
